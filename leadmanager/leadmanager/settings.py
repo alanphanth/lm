dee827ee03fa86b276f9930932b0da5d12166005
Alan Phan <alanphanxyz@gmail.com>
2022-11-14 14:02:50 +0700

    sprint 2 | react

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "leads",
-    "rest_framework",
+    "leads",  # new
+    "rest_framework",  # new
+    "frontend",  # new
 ]
 
 MIDDLEWARE = [
